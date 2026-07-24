bbe6c9338297fb3d9a213699f977087c2f31bdc5
Rizky0203 <rizkynurdiansyah888@gmail.com>
2026-07-24 14:44:18 +0700

    Fix blank screen render

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'))
+  res.sendFile(__dirname + '/index.html')
 })
 
 app.post('/api/registration', async (req, res) => {
